c5b3b596bab8f722d2001b75fa5930f50f7199b2
Marco Whyte <marcolwhyte@gmail.com>
2024-03-03 12:42:03 -0500

    fix social links, noreferrer

--- src/app/components/SocialIcons.tsx
+++ src/app/components/SocialIcons.tsx
@@ -36,7 +36,12 @@ type IconLinkProps = {
 
 const IconLink = ({ icon: Icon, ...props }: IconLinkProps) => {
   return (
-    <Link className='group -m-1 p-1' {...props}>
+    <Link
+      className='group -m-1 p-1'
+      target='_blank'
+      rel='noopener noreferrer'
+      {...props}
+    >
       <Icon className='h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300' />
     </Link>
   );
@@ -47,12 +52,12 @@ const SocialIcons = () => {
     <>
       <IconLink
         icon={GitHubIcon}
-        href='https://github.com/marcowhyte'
+        href='https://github.com/marwhyte'
         aria-label='GitHub'
       />
       <IconLink
         icon={LinkedInIcon}
-        href='https://www.linkedin.com/in/marcowhyte/'
+        href='https://www.linkedin.com/in/marco-whyte/'
         aria-label='LinkedIn'
       />
     </>
