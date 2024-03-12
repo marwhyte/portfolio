925c3d4f27764e5cc8a1d76f90656cce7935f2fa
Marco Whyte <marcolwhyte@gmail.com>
2024-03-12 17:15:02 -0400

    some color adjustments

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -19,27 +19,30 @@ const Post = ({ post }: { post: PostData }) => {
   console.log(formattedDate);
 
   return (
-    <article className='md:grid md:grid-cols-4 md:items-baseline'>
-      <Card className='md:col-span-3'>
-        <Card.Title href={`/blog/${post.slug}`}>{post.title}</Card.Title>
+    <>
+      <div className='absolute -left-1.5 top-2 flex h-3 w-3 items-center justify-center rounded-full border-2 border-zinc-200 bg-white dark:border-zinc-700 dark:bg-zinc-800' />
+      <article className='md:grid md:grid-cols-4 md:items-baseline'>
+        <Card className='md:col-span-3'>
+          <Card.Title href={`/blog/${post.slug}`}>{post.title}</Card.Title>
+          <Card.Eyebrow
+            as='time'
+            dateTime={post.date}
+            className='md:hidden'
+            decorate
+          >
+            {formattedDate}
+          </Card.Eyebrow>
+          <Card.Cta>Read post</Card.Cta>
+        </Card>
         <Card.Eyebrow
           as='time'
           dateTime={post.date}
-          className='md:hidden'
-          decorate
+          className='hidden md:block'
         >
           {formattedDate}
         </Card.Eyebrow>
-        <Card.Cta>Read post</Card.Cta>
-      </Card>
-      <Card.Eyebrow
-        as='time'
-        dateTime={post.date}
-        className='mt-1 hidden md:block'
-      >
-        {formattedDate}
-      </Card.Eyebrow>
-    </article>
+      </article>
+    </>
   );
 };
 
@@ -48,16 +51,7 @@ const Posts = ({ posts }: { posts: PostData[] }) => {
     <ul className='flex max-w-3xl flex-col space-y-16 md:border-l md:border-zinc-300 md:pl-6 md:dark:border-zinc-100/40'>
       {posts.map((post) => (
         <>
-          <li key={post.slug}>
-            <Post post={post} />
-          </li>
-          <li key={post.slug}>
-            <Post post={post} />
-          </li>
-          <li key={post.slug}>
-            <Post post={post} />
-          </li>
-          <li key={post.slug}>
+          <li className='relative' key={post.slug}>
             <Post post={post} />
           </li>
         </>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -18,7 +18,7 @@ const Projects = () => {
     <div style={{ scrollMarginTop: '75px' }} id='projects'>
       <div className='flex flex-col gap-16'>
         {projects.map((project) => (
-          <Card as='li' key={project.name}>
+          <Card key={project.name}>
             <div className='relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0'>
               <Image src={project.logo} alt='' className='h-8 w-8' />
             </div>
