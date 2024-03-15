15657a45e003d912d13fee214269b490388c2848
Marco Whyte <marcolwhyte@gmail.com>
2024-03-15 22:08:44 +0900

    remove client side useeffect for views

--- src/app/blog/[slug]/views.tsx
+++ src/app/blog/[slug]/views.tsx
@@ -1,16 +1,7 @@
-'use client';
-
 import { formatViews } from '@/app/utils';
 import { PostData } from '@/lib/posts';
-import { useEffect } from 'react';
-import { createView } from '../serverActions';
 
 const Views = ({ post }: { post: PostData }) => {
-  useEffect(() => {
-    createView(post.slug);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <span className='relative z-10 text-xs !text-gray-400 dark:!text-gray-400'>
       {formatViews(post.views)}
