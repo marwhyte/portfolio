261f09155fcc211169904763c752a45607f90f8a
Marco Whyte <marcolwhyte@gmail.com>
2024-03-26 15:54:10 +0700

    change up upstash implementation

--- src/app/blog/serverActions.ts
+++ src/app/blog/serverActions.ts
@@ -1,21 +1,15 @@
-// Assuming the top of the file indicates this is server-side code
 'use server';
 
-// Import necessary utilities and configurations
-import redis from '@/app/redis'; // Adjust this import path as necessary
+import { Redis } from '@upstash/redis';
+
 import { revalidateTag } from 'next/cache';
 
 // This function increments the view count for a given post
 export async function createView(postSlug: string): Promise<void> {
+  const redis = Redis.fromEnv();
+
   // Increment the view count in Redis using the post's slug
   await redis.incr(`post_views:${postSlug}`);
   // Revalidate the post cache, if you're using ISR or similar caching strategies
   revalidateTag('posts');
-}
-
-export async function getAllViews() {
-  const redisKeys = await redis.keys('post_views:*');
-  const viewsArray: string[] = await redis.mget(...redisKeys);
-
-  return viewsArray;
 }
