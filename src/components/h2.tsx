3ed37ed1826097fc821afda4c6fe91e11e60c1d0
Marco Whyte <marcolwhyte@gmail.com>
2024-06-01 16:31:43 -0400

    the uyuni tour

--- src/components/h2.tsx
+++ src/components/h2.tsx
@@ -3,7 +3,7 @@ import { withHeadingId } from './utils';
 
 export const H2 = ({ children }: { children?: ReactNode }) => {
   return (
-    <h2 className='group relative my-8 text-xl font-bold'>
+    <h2 className='group relative my-8 -mb-2 text-xl font-bold'>
       {withHeadingId(children)}
     </h2>
   );
