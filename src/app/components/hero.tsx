6db9e376ffe0e087a9b890113e270f25e37a8071
Marco Whyte <marcolwhyte@gmail.com>
2025-03-26 21:40:28 -0400

    add world map

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -4,22 +4,19 @@ import SocialIcons from './social-icons';
 import Image from 'next/image';
 import { ArrowDownCircleIcon } from '@heroicons/react/24/outline';
 import Button from './button';
+import { WavyBackground } from './wavy-background';
 
 const Hero = () => {
   return (
     <div className='relative isolate min-h-screen bg-white pt-14 dark:bg-gray-900'>
-      <div
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-        aria-hidden='true'
-      >
-        <div
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
+      <WavyBackground
+        containerClassName='absolute inset-0 -z-10'
+        colors={['#ff80b5', '#9089fc', '#6366f1', '#8b5cf6']}
+        waveOpacity={0.3}
+        blur={20}
+        speed='slow'
+      />
+
       <div className='pb-24 lg:pb-40 lg:pt-24'>
         <div className='mx-auto max-w-7xl px-6 lg:px-8'>
           <div className='mx-auto max-w-2xl text-center'>
