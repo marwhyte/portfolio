fbcb4a2f4d2705c9b06ae8f2fed4e11df7f12721
Marco Whyte <marcolwhyte@gmail.com>
2025-04-01 10:54:34 -0400

    change to wand.email

--- src/app/components/projects.tsx
+++ src/app/components/projects.tsx
@@ -1,17 +1,17 @@
 import Card from './card';
 import graphMakerLogo from '../../../public/graph-maker-logo.png';
-import sentSwiftlyLogo from '../../../public/sent-swiftly-logo.png';
+import wandLogo from '../../../public/wand-logo.png';
 import Image from 'next/image';
 import { LinkIcon } from '@heroicons/react/20/solid';
 
 const Projects = () => {
   const projects = [
     {
-      name: 'SentSwiftly',
+      name: 'wand.email',
       description:
-        'SentSwiftly is an easy, drag-and-drop email builder that allows you to create beautiful emails in minutes, not hours. It integrates easily into most tech stacks, including html, react, and nextJS',
-      link: { href: 'https://sentswiftly.com', label: 'sentswiftly.com' },
-      logo: sentSwiftlyLogo,
+        'wand.email is an easy, drag-and-drop email builder that allows you to create beautiful emails in minutes, not hours. It integrates easily into most tech stacks, including html, react, and nextJS',
+      link: { href: 'https://wand.email', label: 'wand.email' },
+      logo: wandLogo,
     },
     {
       name: 'GraphMaker',
